15bc2f01267e870c7b3528f66c3ae033329b6659
Andrew S <andrews54757@gmail.com>
2017-04-19 13:19:27 -0400

    Update writer.js

--- FWW-5/writer.js
+++ FWW-5/writer.js
@@ -1,61 +1,35 @@
-var numbers = [4397];
+var numbers = [4397,60000];
 var results = [];
 
 for (var i = 0; i < numbers.length; i ++) {
 var current = numbers[i];
-if (current < 128) { // one byte
+var num;
+if (current < 128) {
+results.push(current)
+} else {;
 
-results.push(current);
-
-} else if (current < 4224) { // two bytes
-var c = current - 128;
-
-var b = c & 0x7F
-
-var d = Math.floor((c - b) / 128);
-results.push(d + 128)
-results.push(b)
-} else if (current < 528512) { // three bytes
-var c = current - 4224;
-
-var b = c & 0x7F;
-var f = Math.floor((c - b) / 128) & 0x7F;
-var d = Math.floor((c - b) / 256);
-results.push(d + 160)
-results.push(f)
-results.push(b)
-
-} else if (current < 67637376) { // Four bytes
-var c = current - 528512;
-
-var b = c & 0x7F; // last byte
-var g = Math.floor((c - b) / 128) & 0x7F;
-var f = Math.floor((c - b) / 256) & 0x7F;
-var d = Math.floor((c - b) / 512); // first byte
-results.push(d + 160)
-results.push(f)
-results.push(g)
-results.push(b)
-
-} else { // five bytes
-var c = current - 67637376;
-  
-var b = c & 0x7F; // last byte
-var g = Math.floor((c - b) / 128) & 0x7F;
-var f = Math.floor((c - b) / 256) & 0x7F;
-var h = Math.floor((c - b) / 512) & 0x7F;
-var d = Math.floor((c - b) / 1024); // first byte
-results.push(d + 160)
-results.push(h);
-results.push(f)
-results.push(g)
-results.push(b)
-
+var cache = [128,4224,528512,67637376]
+if (current < 4224) {
+num = current - 128
+var len = 1;
+} else if (current < 528512) {
+num = current - 4224
+var len = 2;
+} else if (current < 67637376) {
+num = current - 528512
+var len = 3;
+} else {
+num = current - 67637376
+var len = 4;
 }
 
+var add = 128 + ((len - 1) << 5);
 
+var r = results.length;
+for (var k=0; k < len; k++) {
+results[r + len - k] = num & 0x7F; // last 7 bits
+num = Math.floor(num / 128) // right shift
 }
-
-console.log(results)
-
-
+results[r] = num + add;
+}
+}
